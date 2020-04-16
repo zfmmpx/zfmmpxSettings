915be8b865476c48b4a11b14d4797d1675a46650
jimmy-huang <jimmy.huang@binance.com>
2020-04-17 03:24:16 +0800

    prettier all file 2

---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  trailingComma: 'all',
+  tabWidth: 2,
+  semi: false,
+  singleQuote: true,
+  jsxBracketSameLine: false,
+  arrowParens: 'always',
+  endOfLine: 'lf',
+  printWidth: 100,
+}
